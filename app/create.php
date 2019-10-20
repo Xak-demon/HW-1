7122c6a2ee41367e381764a0028d59d54ee80d1d
Andrew Sergeevich <Xakdem4@gmail.com>
2019-10-20 03:51:42 +0300

    Added create product feature

--- app/create.php
+++ app/create.php
@@ -1,2 +1,22 @@
 <?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
 echo "create";
+
+$product = new \Xak\Core\Product( 'Mouse', 'PC', 11, 6);
+
+$productResult = $product->createProduct();
+
+if($productResult){
+	echo "\n was create product:\n";
+	foreach ($productResult as $key => $value){
+		echo "$key = $value\n";
+	}
+}else{
+	echo "Oops, something went wrong";
+}
+
+
+
+
--- src/Product.php
+++ src/Product.php
@@ -5,4 +5,127 @@ namespace Xak\Core;
 
 class Product {
 
+	private $id;
+	private $name;
+	private $category;
+	private $price;
+	private $quantity;
+
+	/**
+	 * Product constructor.
+	 *
+	 * @param $name
+	 * @param $category
+	 * @param $price
+	 * @param $quantity
+	 */
+	public function __construct( $name, $category, $price, $quantity ) {
+		$this->name     = $name;
+		$this->category = $category;
+		$this->price    = $price;
+		$this->quantity = $quantity;
+		$this->setId();
+	}
+
+
+	public function getId() {
+		return $this->id;
+	}
+
+
+	public function setId() {
+
+		$productList = $this->getProductList();
+		$lastId = ( empty( $productList ) ) ? 0 : $productList[ count( $productList ) - 1 ]['id'];
+		$this->id = ++ $lastId;
+
+	}
+
+	public function getName() {
+		return $this->name;
+	}
+
+	public function setName( $name ) {
+		$this->name = $name;
+	}
+
+	public function getCategory() {
+		return $this->category;
+	}
+
+	public function setCategory( $category ) {
+		$this->category = $category;
+	}
+
+	public function getPrice() {
+		return $this->price;
+	}
+
+	public function setPrice( $price ) {
+		$this->price = $price;
+	}
+
+	public function getQuantity() {
+		return $this->quantity;
+	}
+
+	public function setQuantity( $quantity ) {
+		$this->quantity = $quantity;
+	}
+
+	public function move() {
+
+	}
+
+	public function getProduct( $id ) {
+		$file = file_get_contents( __DIR__ . '/../data/databese.json' );
+		$productList = json_decode( $file, true );
+		$productList = ( is_array( $productList ) ) ? $productList : array();
+		unset( $file );
+
+	}
+
+	private function saveProduct( $data ) {
+
+		$productList['Products'] = $data;
+
+		return file_put_contents( __DIR__ . '/../data/databese.json', json_encode( $productList ) );
+	}
+
+	public function createProduct() {
+
+		$data = array(
+			'id'       => $this->id,
+			'name'     => $this->name,
+			'category' => $this->category,
+			'price'    => $this->price,
+			'quantity' => $this->quantity
+		);
+
+		$productList = $this->getProductList();
+
+		$productList = ( is_array( $productList ) ) ? $productList : array();
+
+		$productList[] = $data;
+
+		$result = $this->saveProduct( $productList );
+
+		if ( $result ) {
+			return $data;
+		} else {
+			return false;
+		}
+
+	}
+
+	private function getProductList() {
+
+		$file = file_get_contents( __DIR__ . '/../data/databese.json' );
+
+		$productList = json_decode( $file, true );
+
+		unset( $file );
+
+		return $productList['Products'];
+	}
 }
